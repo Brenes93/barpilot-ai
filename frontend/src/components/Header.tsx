8ba34293ee01d93c0ed0833b56640912de08f217
Brenes93 <brenes93@hotmail.es>
2026-06-06 20:49:01 +0200

    feat: create reusable Header component with props

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header(props) {
+
+  return (
+      <section id="center">
+        <div>
+          <h1>{props.titulo}</h1>
+          <p>Mi primer proyecto Full Stack para aprender programación.</p>
+        </div>
+      </section>
+  )
+}
+
+export default Header
